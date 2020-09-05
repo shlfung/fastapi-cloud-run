33908133b27e5803ca729cd4012316d935c34158
shlfung <stephen.shlfung@gmail.com>
2020-09-04 19:31:39 -0700

    writing function to return recalls

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM tiangolo/uvicorn-gunicorn:python3.8-slim
-RUN pip install --no-cache-dir fastapi google-cloud-storage google-cloud-firestore
+RUN pip install --no-cache-dir fastapi google-cloud-storage google-cloud-firestore google-cloud-logging
 COPY ./app /app
--- app/main.py
+++ app/main.py
@@ -1,16 +1,35 @@
-from typing import Optional
+from typing import Optional, List
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 from datetime import datetime
 from pytz import timezone
-import requests, json
+import requests, json, os
 
-from google.cloud import firestore, storage
+from google.cloud import firestore, storage, logging
 
 
 
 app = FastAPI()
+
+
+
+class Recall(BaseModel):
+    id: int
+    category: List[int]
+    datePublished: Optional[datetime] = None
+    title: str
+    url: str
+
+recalls = {
+    "id" : 73731, 
+    "category" : [4, 5],
+    "title": "Hi-Lift Storage Hoist Classic and Hi-Lift Storage Hoist Pro recalled due to injury hazard", 
+    "url" :"/api/73731/en"
+}
 
 # This is to to get the recalls and write to the database everyday
 # Cloud function run this once a day
@@ -50,11 +69,41 @@ def write_recalls():
     
     return {"Data" : "cannot be recorded"}
 
+@app.get("/recalls")
+def get_recalls():
+
+   
+
+    print('Inside Get Recalls')
+    
+
+    db = firestore.Client()
+    #start_date = datetime.datetime(2020, 9, 1, 0, 0)
+    #print('Start Date is', start_date)
+    #doc_ref = db.collection('recalls').document('73731')
+    docs = db.collection('recalls').stream()
+    #doc = doc_ref.get()
+    #docs = doc_ref
+    for doc in docs:
+        print(f'{doc.id} => {doc.to_dict()}')
+    print("This is the end")
+    #if docs.exists:
+    #    print(f'Document data: {docs.to_dict()}')
+    #else:
+    #    print('No Doc Ref')
+    #test_recall = Recall(id=73731, category=Category(categoryId=[4]), title="Something", url="/api/73731/en")
+    #json_compatible_data = jsonable_encoder(doc.to_dict())
+    #return JSONResponse(content=json_compatible_data)
+    return {"Completed" : "Request'"}
+
+ 
+
         
 # This is to confirm Google Cloud Run is working
 @app.get("/")
 def read_root():
-    return {"Hello": "World2"}
+    print('Testing if Deployment is working')
+    return {"Hello": "World3"}
 
 #This is to test if I can use Google Cloud Run to control other Google Services
 @app.get("/test-storage")
